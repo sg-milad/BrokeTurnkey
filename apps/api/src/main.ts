e1e0684a45289eaad0a0cc9ee08c6c14d5d6a4a2
milad <miladbachari14@gmail.com>
2026-07-18 20:52:05 +0330

    feat: add swagger to main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BrokeTurnkey API')
+    .setDescription('The API documentation for BrokeTurnkey')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
