2e0883b8da91b77ee8c2feab832a87b7982ed17b
milad <miladbachari14@gmail.com>
2026-07-19 18:03:10 +0330

    fix some bugs

--- apps/api/src/config/app.config.ts
+++ apps/api/src/config/app.config.ts
@@ -11,10 +11,4 @@ export const appConfig = registerAs('app', () => ({
         user: process.env.DB_USER ?? 'postgres',
         password: process.env.DB_PASSWORD ?? 'postgres',
     },
-
-    vault: {
-        addr: process.env.VAULT_ADDR ?? 'http://localhost:8200',
-        roleId: process.env.VAULT_ROLE_ID,
-        secretId: process.env.VAULT_SECRET_ID,
-    },
 }));
